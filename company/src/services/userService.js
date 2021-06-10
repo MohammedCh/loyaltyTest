713c556437a09f125e421245c3c3a4a5c7221cc9
Mohammed Chakmakchi <mohammed.chakmakchi@lego.com>
2021-06-10 16:26:15 +0200

    up

--- company/src/services/userService.js
+++ company/src/services/userService.js
@@ -33,30 +33,7 @@ function userService() {
             console.error(error);
         }
     }
-    //addTransaction("helloword1", "5", "200");
-    //function getUserByEmail(email) {
-    //     const options = {
-    //         host: 'swapi.dev',
-    //         path: '/api/people/1',
-    //         port: 80,
-    //         method: 'GET'
-    //     }
 
-    //     const request = http.request(options, function (response) {
-    //         var body = ""
-    //         response.on('data', function (data) {
-    //             body += data;
-    //         });
-    //         response.on('end', function () {
-    //             //res.send(JSON.parse(body));
-    //             console.log(body);
-    //         });
-    //     });
-    //     request.on('error', function (e) {
-    //         console.log('Problem with request: ' + e.message);
-    //     });
-    //     request.end();
-    // }
     return { getUserBalanceByEmail, addTransaction };
 };
 
